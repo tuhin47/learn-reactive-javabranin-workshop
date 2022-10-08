1745937ed27d3d13e1442c3230aa47f4ab393c6a
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2022-10-08 07:45:33 +0600

    solve Exercise4
    Took 3 minutes

--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -9,10 +9,13 @@ public class Exercise4 {
         // Use ReactiveSources.intNumberMono()
 
         // Print the value from intNumberMono when it emits
-        // TODO: Write code here
+        System.out.println("\n// Print the value from intNumberMono when it emits\n");
+        ReactiveSources.intNumberMono().subscribe(System.out::println);
 
         // Get the value from the Mono into an integer variable
-        // TODO: Write code here
+        System.out.println("\n// Get the value from the Mono into an integer variable\n");
+        Integer block = ReactiveSources.intNumberMono().block();
+        System.out.println(block);
 
         System.out.println("Press a key to end");
         System.in.read();
