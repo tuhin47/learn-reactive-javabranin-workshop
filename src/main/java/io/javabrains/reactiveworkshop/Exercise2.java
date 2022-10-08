f2c75842602523e41e420c9e8241d26ab4c50919
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2022-10-08 07:34:47 +0600

    complete exercise 2
    Took 16 minutes

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -9,10 +9,14 @@ public class Exercise2 {
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
+        System.out.println("\nPrint all numbers in the ReactiveSources.intNumbersFlux stream:\n");
+        ReactiveSources.intNumbersFlux().subscribe(System.out::println);
+
 
         // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
+        System.out.println("\nPrint all users in the ReactiveSources.userFlux stream:\n");
+        ReactiveSources.userFlux().subscribe(System.out::println);
+
 
         System.out.println("Press a key to end");
         System.in.read();
