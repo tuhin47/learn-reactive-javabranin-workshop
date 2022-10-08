c7e0868a2788b27f2fd405a10421c9d9600e402a
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2022-10-08 07:57:53 +0600

    solve Exercise6.java
    Took 4 minutes

--- src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
@@ -1,6 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
+import java.time.Duration;
 
 public class Exercise6 {
 
@@ -10,7 +11,13 @@ public class Exercise6 {
         // Use ReactiveSources.unresponsiveFlux() and ReactiveSources.unresponsiveMono()
 
         // Get the value from the Mono into a String variable but give up after 5 seconds
-        // TODO: Write code here
+        try {
+            System.out.println(ReactiveSources.unresponsiveFlux().blockFirst(Duration.ofSeconds(3)));
+        } catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
+        }
+
+
 
         // Get the value from unresponsiveFlux into a String list but give up after 5 seconds
         // Come back and do this when you've learnt about operators!
