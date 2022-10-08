a3e69d96c3385cdea1c1c22b80928aa032504e27
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2022-10-08 08:22:12 +0600

    solve Exercise 8
    Took 8 minutes

--- src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise8.java
@@ -3,6 +3,7 @@ package io.javabrains.reactiveworkshop;
 import reactor.core.publisher.Flux;
 
 import java.io.IOException;
+import java.time.Duration;
 
 public class Exercise8 {
 
@@ -12,14 +13,20 @@ public class Exercise8 {
         // Use ReactiveSources.intNumbersFluxWithException()
 
         // Print values from intNumbersFluxWithException and print a message when error happens
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException().subscribe(
+                integer -> System.out.println("integer = " + integer),
+                throwable -> System.out.println("throwable.getMessage() = " + throwable.getMessage())
+        );
 
         // Print values from intNumbersFluxWithException and continue on errors
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException().onErrorContinue((throwable, o) -> System.out.println("throwable = " + throwable.getMessage()))
+                .subscribe(
+                        integer -> System.out.println("integer = " + integer));
 
         // Print values from intNumbersFluxWithException and when errors
         // happen, replace with a fallback sequence of -1 and -2
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithException().onErrorResume(throwable -> Flux.just(-1, -2).delayElements(Duration.ofMillis(500)))
+                .subscribe(integer -> System.out.println("integer = " + integer));
 
         System.out.println("Press a key to end");
         System.in.read();
