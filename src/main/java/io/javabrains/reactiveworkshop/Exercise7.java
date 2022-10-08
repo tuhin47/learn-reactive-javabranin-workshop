5e50618aebfdcb5ae40532c289529501620312ed
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2022-10-08 08:13:54 +0600

    solve Exercise7
    Took 16 minutes

--- src/main/java/io/javabrains/reactiveworkshop/Exercise7.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise7.java
@@ -10,25 +10,24 @@ public class Exercise7 {
         // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
 
         // Print all values from intNumbersFlux that's greater than 5
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().log().filter(integer -> integer > 5).subscribe(System.out::println);
 
         // Print 10 times each value from intNumbersFlux that's greater than 5
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().filter(integer -> integer > 5).map(integer -> integer*10).subscribe(System.out::println);
 
         // Print 10 times each value from intNumbersFlux for the first 3 numbers emitted that's greater than 5
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().filter(integer -> integer > 5).take(3).subscribe(integer -> System.out.println("integer = " + integer));
 
         // Print each value from intNumbersFlux that's greater than 20. Print -1 if no elements are found
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().filter(integer -> integer > 20).defaultIfEmpty(-1).subscribe(integer -> System.out.println("integer = " + integer));
 
         // Switch ints from intNumbersFlux to the right user from userFlux
-        // TODO: Write code here
-
+        ReactiveSources.intNumbersFlux().flatMap(integer -> ReactiveSources.userFlux().filter(user -> integer.equals(user.getId()))).subscribe(user -> System.out.println("user = " + user));
         // Print only distinct numbers from intNumbersFluxWithRepeat
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithRepeat().distinct().subscribe(integer -> System.out.println("integer = " + integer));
 
         // Print from intNumbersFluxWithRepeat excluding immediately repeating numbers
-        // TODO: Write code here
+        ReactiveSources.intNumbersFluxWithRepeat().distinctUntilChanged().subscribe(integer -> System.out.println("integer = " + integer));
 
         System.out.println("Press a key to end");
         System.in.read();
