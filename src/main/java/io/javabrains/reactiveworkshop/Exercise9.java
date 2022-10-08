824f1938647b0eebe09b5fe45051c9c4ca7fbb82
MD Towhidul Islam <tuhintowhidul9@gmail.com>
2022-10-08 08:31:21 +0600

    solve Exercise9.java
    Took 9 minutes

--- src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
@@ -10,13 +10,16 @@ public class Exercise9 {
         // Use ReactiveSources.intNumbersFlux()
 
         // Print size of intNumbersFlux after the last item returns
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().count().subscribe(aLong -> System.out.println("aLong = " + aLong));
 
         // Collect all items of intNumbersFlux into a single list and print it
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().collectList().subscribe(integers -> System.out.println("integers = " + integers));
 
         // Transform to a sequence of sums of adjacent two numbers
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux()
+                .buffer(2)
+                .map(integers -> integers.stream().reduce(0, (integer, integer2) -> integer + integer2))
+                .subscribe(integer -> System.out.println("integer = " + integer));
 
         System.out.println("Press a key to end");
         System.in.read();
